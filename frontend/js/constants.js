625f8471a609bc3370e676d293b606bdf99df981
Mike <1creative.mts@gmail.com>
2022-03-23 14:11:19 -0400

    updated info and stule

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,24 +1,24 @@
 const contractAddress = "0x80d7a96B7A3c057F81f452B7f24873A75fCF094e"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to Slow Alien Car Club!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
-const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
+const welcome_p = 'Slow Alien Car Club is a membership program that gives you immediate benefits to MANY luxury car rentals and more! You can find more specific information at the <a href="https://quartermileexotics.com/sacc"  target="_blank" rel="noopener noreferrer"> Quartermile Exotics SACC </a> website.';
+const h1_presale_coming_soon = "Dropping April 4th!";
+const h1_presale_mint = null;
+const h1_public_mint = "SACC has finally launched!";
+const h2_presale_coming_soon = null;
+const h2_presale_mint = "Time 'til launch..";
+const p_presale_coming_soon = "The Earth beacon has located the Galactic Elite and is attempting to communicate. Stay tuned for updates!";
+const p_presale_mint_not_whitelisted = null;
+const p_presale_mint_whitelisted = null;
+const p_presale_mint_already_minted = null;
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
-const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
-const button_presale_mint_not_whitelisted = "Get on the Whitelist";
+const button_presale_coming_soon = null;
+const button_presale_mint_whitelisted = null;
+const button_presale_mint_not_whitelisted = null;
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_public_mint = "Mint My Alien!";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
