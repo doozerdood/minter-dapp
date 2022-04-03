c600955ecb286f07df7a8e1f5b1fb1950139c277
Mike <1creative.mts@gmail.com>
2022-04-03 16:51:31 -0400

    hope

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,12 +67,12 @@ const PUBLIC_MINT_START_DATE = "2022-04-04T19:00:48+00:00"; // This is required.
 const PRESALE_MINT_PRICE = 0; 
 const PRESALE_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xb0d316cb756304bB8a4a0726aF8e660cAf370f18"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0x91b148ba73368449EE28e55A3857bD730fcE9bc3"; // Address that will receive the royalty
 const BASE_URI = "ipfs://bafybeieky77mf6fomnpmyticyxhqceyolw5nffg3n3qapclpsiz5llwdte/"; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = ["0xb0d316cb756304bB8a4a0726aF8e660cAf370f18", "0x91b148ba73368449EE28e55A3857bD730fcE9bc3", "0xb3368a88572971766fc2a6658514b6c4A1168d38"]; // only update if you want to manually set the whitelisted addresses
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x1c9aA5c718c4Aa1B58c09c2978E14721af23F25B"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
